497eabf90e11db508a5008a906904d75cda6dd39
rodrigodojo <rodrigodoj@gmail.com>
2021-03-08 17:24:00 -0300

    Exemplo de conexão com MySQL com inserção

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,53 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		
 		try {
-			conn = DB.getConnetion();
-			st = conn.createStatement();
-			rs = st.executeQuery("SELECT * from department");
+			conn =DB.getConnetion();
 			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name,Email,BirthDate,BaseSalary,DepartmentId)"
+			+"VALUES"+"(?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1,"Carl Purple");
+			st.setString(2, "carl@mgail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000);
+			st.setInt(5,4);
+			
+		
+			st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = "+ id);
+				}
+			}else {
+				System.out.println("no Rows affected!");
 			}
 			
-		}catch(SQLException e){
+		}catch(SQLException | ParseException e){
 			e.printStackTrace();
 		}finally {
-		 DB.closeResultSet(rs);
 		 DB.closeStatement(st);
 		 DB.closeConnection();
 		}
